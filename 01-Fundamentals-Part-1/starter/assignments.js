9b6e20ebdfdf904e55fb2f9f2762fc7538476d19
Johann Molano <jjdmolano@gmail.com>
2021-05-16 21:26:58 +0800

    Added assignment and lecture notes for Basic Operators

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,22 +1,38 @@
 // Values & Variables
-/* const country = 'Philippines';
+const country = 'Philippines';
 const continent = 'Asia';
-let population = 110590000;
+let population = 110;
 
-console.log(country);
-console.log(continent);
+// console.log(country);
+// console.log(continent);
 // used toLocaleString to display integer with commas
-console.log(population.toLocaleString());
-
+// console.log(population.toLocaleString());
 
 // Data Types
 const isIsland = true;
-const language;
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
-*/
+let language;
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
 
 // let, const and var
 language = 'Tagalog';
+
+// Basic Operators
+
+let halfPopulation = population / 2;
+console.log(halfPopulation);
+population++;
+console.log(population);
+
+let finlandPopulation = 6;
+finlandComparison = population > finlandPopulation;
+console.log(finlandComparison);
+
+let averagePopulation = 33;
+averageComparison = population < averagePopulation;
+console.log(averageComparison);
+
+let description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+console.log(description);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -6,6 +6,7 @@ console.log(eq);
 console.log('Jonas');
 console.log(23);
 
+// Values and Variables
 let firstName = 'Matilda';
 console.log(firstName);
 
@@ -23,6 +24,7 @@ console.log(myFirstJob);
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
+// Data Types
 // console.log(typeof true);
 console.log(typeof javascriptIsFun);
 // console.log(typeof 23);
@@ -31,6 +33,7 @@ console.log(typeof javascriptIsFun);
 javascriptIsFun = 'YES!';
 console.log(typeof javascriptIsFun);
 
+// let const and var
 let year;
 console.log(year);
 console.log(typeof year);
@@ -50,3 +53,34 @@ job = 'teacher';
 lastName = 'Molano';
 console.log(lastName);
 */
+
+// Basic Operators
+// Math Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + " " + lastName);
+console.log(`${firstName} ${lastName}`); //Template Literal version
+
+// Assignment Operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x  = x + 1
+x--; // x = x - 1
+x--;
+console.log(x);
+
+// Comparison Operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
