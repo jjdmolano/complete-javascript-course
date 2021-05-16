d9be0d633f1138b1450897784bf146d3b6607c8f
Johann Molano <jjdmolano@gmail.com>
2021-05-16 20:13:21 +0800

    Added assignment and lecture notes for Data Types

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,3 +1,4 @@
+// Values & Variables
 let country = 'Philippines';
 let continent = 'Asia';
 let population = 110590000;
@@ -6,3 +7,12 @@ console.log(country);
 console.log(continent);
 // used toLocaleString to display integer with commas
 console.log(population.toLocaleString());
+
+
+// Data Types
+let isIsland = true;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-let js = 'amazing';
+/* let js = 'amazing';
 // if(js === 'amazing') alert('Javascript is fun!');
 let eq = 40 + 8 + 23 - 10;
 console.log(eq);
@@ -18,5 +18,24 @@ let PI = 3.1415;
 // variables should be descriptive for easier code readability
 let myFirstJob = 'Programmer';
 let myCurrentJob = 'Teacher';
-
 console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+console.log(typeof null);
