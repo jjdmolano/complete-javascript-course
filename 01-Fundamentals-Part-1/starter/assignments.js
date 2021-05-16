2bd308b432706b6462cfec1181617c34495777c7
Johann Molano <jjdmolano@gmail.com>
2021-05-16 19:45:42 +0800

    Added first assignment and lecture notes for Values and Variables

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,8 @@
+let country = 'Philippines';
+let continent = 'Asia';
+let population = 110590000;
+
+console.log(country);
+console.log(continent);
+// used toLocaleString to display integer with commas
+console.log(population.toLocaleString());
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,22 @@
-        let js = 'amazing';
-        if(js === 'amazing') alert('Javascript is fun!');
-        let eq = 40 + 8 + 23 - 10;
-        console.log(eq);
+let js = 'amazing';
+// if(js === 'amazing') alert('Javascript is fun!');
+let eq = 40 + 8 + 23 - 10;
+console.log(eq);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';
+console.log(firstName);
+
+// variables can start with or include: $ or _
+let $function = 27;
+let jonas_matilda = 'JM';
+let person = 'jonas';
+let PI = 3.1415;
+
+// variables should be descriptive for easier code readability
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+console.log(myFirstJob);
