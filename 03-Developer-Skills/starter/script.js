c1d25d2f5276eb01e2e727f99a64aec95573c3db
Johann Molano <jjdmolano@gmail.com>
2021-05-17 23:07:23 +0800

    Added learning how to code notes and updated node, npm and packages

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,15 @@
-// Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const calcAge = birthYear => 2037 - birthYear;
+
+console.log(calcAge(1991));
+
+// Set a goal!
+    // specific, measurable, realistic and time-based
+// Don't copy-paste code, understand it
+// Reinforce newly learned code, try it and use it
+    // challenge yourself
+// Practice, practice, practice
+// You can always refactor code later
+// Embrace the fact that you will never know everything
+// Don't lose motivation!
